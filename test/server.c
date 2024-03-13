d1be3d0adf0628954f31153694e52ef49fc0ef7f
Santiago Cordova <98288513+ScordovaB@users.noreply.github.com>
2024-03-13 14:21:01 -0600

    bidirectional

--- test/server.c
+++ test/server.c
@@ -9,24 +9,20 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define PORT 3490
+#define PORT 5000
 #define BACKLOG 10
+#define MAXSIZE 1024
 
-int main()
-{
+int main() {
     struct sockaddr_in server;
     struct sockaddr_in dest;
-    int status,socket_fd, client_fd,num;
+    int status, socket_fd, client_fd, num;
     socklen_t size;
 
-    char buffer[10241];
-    char *buff;
-//  memset(buffer,0,sizeof(buffer));
-    int yes =1;
+    char buffer[MAXSIZE];
+    int yes = 1;
 
-
-
-    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0))== -1) {
+    if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         fprintf(stderr, "Socket failure!!\n");
         exit(1);
     }
@@ -35,60 +31,56 @@ int main()
         perror("setsockopt");
         exit(1);
     }
+
     memset(&server, 0, sizeof(server));
-    memset(&dest,0,sizeof(dest));
+    memset(&dest, 0, sizeof(dest));
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
-    server.sin_addr.s_addr = INADDR_ANY; 
-    if ((bind(socket_fd, (struct sockaddr *)&server, sizeof(struct sockaddr )))== -1)    { //sizeof(struct sockaddr) 
+    server.sin_addr.s_addr = INADDR_ANY;
+
+    if ((bind(socket_fd, (struct sockaddr *)&server, sizeof(struct sockaddr))) == -1) {
         fprintf(stderr, "Binding Failure\n");
         exit(1);
     }
 
-    if ((listen(socket_fd, BACKLOG))== -1){
+    if ((listen(socket_fd, BACKLOG)) == -1) {
         fprintf(stderr, "Listening Failure\n");
         exit(1);
     }
 
-    wwhile(1) {
-
+    while (1) {
         size = sizeof(struct sockaddr_in);
-
-        if ((client_fd = accept(socket_fd, (struct sockaddr *)&dest, &size))==-1 ) {
+        if ((client_fd = accept(socket_fd, (struct sockaddr *)&dest, &size)) == -1) {
             perror("accept");
             exit(1);
         }
+
         printf("Server got connection from client %s\n", inet_ntoa(dest.sin_addr));
 
-        while(1) {
-
-                if ((num = recv(client_fd, buffer, 1024,0))== -1) {
-                        perror("recv");
-                        exit(1);
-                }
-                else if (num == 0) {
-                        printf("Connection closed\n");
-                        //So I can now wait for another client
-                        break;
-                }
+        // Enter loop to continuously check for messages from client
+        while (1) {
+            // Check for incoming messages from client
+            num = recv(client_fd, buffer, sizeof(buffer), MSG_DONTWAIT);
+            if (num > 0) {
                 buffer[num] = '\0';
-                printf("Server:Msg Received %s\n", buffer);
-                if ((send(client_fd,buffer, strlen(buffer),0))== -1) 
-                {
-                     fprintf(stderr, "Failure Sending Message\n");
-                     close(client_fd);
-                     break;
-                }
+                printf("Server: Msg Received %s\n", buffer);
+            }
 
-                printf("Server:Msg being sent: %s\nNumber of bytes sent: %d\n",b
-                uffer, strlen(buffer));
+            // Handle server-side input
+            // For demonstration purposes, let's read from stdin and send it to the client
+            printf("Server: Enter Data for Client:\n");
+            fgets(buffer, MAXSIZE - 1, stdin);
+            if ((send(client_fd, buffer, strlen(buffer), 0)) == -1) {
+                fprintf(stderr, "Failure Sending Message\n");
+                close(client_fd);
+                exit(1);
+            }
+        }
 
-        } //End of Inner While...
-        //Close Connection Socket
+        // Close Connection Socket
         close(client_fd);
-    } //Outer While
+    }
 
     close(socket_fd);
-    //close(client_fd); 
     return 0;
 }
