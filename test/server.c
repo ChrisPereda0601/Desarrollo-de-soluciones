07507ad4a31d70fb5009af7fc3b5c891fc296eb2
santiago <di730547@iteso.mx>
2024-03-13 19:41:16 -0600

    sever

--- test/server.c
+++ test/server.c
@@ -19,6 +19,8 @@ int main() {
     int status, socket_fd, client_fd, num;
     socklen_t size;
 
+    pid_t p;
+
     char buffer[MAXSIZE];
     int yes = 1;
 
@@ -48,6 +50,8 @@ int main() {
         exit(1);
     }
 
+
+
     while (1) {
         size = sizeof(struct sockaddr_in);
         if ((client_fd = accept(socket_fd, (struct sockaddr *)&dest, &size)) == -1) {
@@ -57,25 +61,41 @@ int main() {
 
         printf("Server got connection from client %s\n", inet_ntoa(dest.sin_addr));
 
-        // Enter loop to continuously check for messages from client
-        while (1) {
-            // Check for incoming messages from client
-            num = recv(client_fd, buffer, sizeof(buffer), MSG_DONTWAIT);
-            if (num > 0) {
-                buffer[num] = '\0';
-                printf("Server: Msg Received %s\n", buffer);
-            }
+        p = fork();
 
-            // Handle server-side input
-            // For demonstration purposes, let's read from stdin and send it to the client
-            printf("Server: Enter Data for Client:\n");
-            fgets(buffer, MAXSIZE - 1, stdin);
-            if ((send(client_fd, buffer, strlen(buffer), 0)) == -1) {
-                fprintf(stderr, "Failure Sending Message\n");
-                close(client_fd);
-                exit(1);
+        if(p<0){
+            printf("Ana maria viene de malas\n");
+            return 1;
+        }else if(p==0){
+            //proceso hijo
+            while(1){
+                num = recv(client_fd, buffer, sizeof(buffer), MSG_DONTWAIT);
+                if (num > 0) {
+                    buffer[num] = '\0';
+                    printf("Server: Msg Received %s\n", buffer);
+                }
             }
         }
+        else{
+            //proceso PADRE
+            printf("Va a entrar al padre\n");
+            while (1) {
+                // Check for incoming messages from client
+
+
+                // Handle server-side input
+                // For demonstration purposes, let's read from stdin and send it to the client
+                printf("Server: Enter Data for Client:\n");
+                fgets(buffer, MAXSIZE - 1, stdin);
+                if ((send(client_fd, buffer, strlen(buffer), 0)) == -1) {
+                    fprintf(stderr, "Failure Sending Message\n");
+                    close(client_fd);
+                    exit(1);
+                }
+            }
+        }
+        // Enter loop to continuously check for messages from client
+
 
         // Close Connection Socket
         close(client_fd);
