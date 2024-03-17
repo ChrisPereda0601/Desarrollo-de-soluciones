6ba1ef5fb1fb8d10ddf726429a23412e7d65be62
Ana Maria <ana.anaya@iteso.mx>
2024-03-17 13:15:32 -0600

    botones

--- test/botones/botones.c
+++ test/botones/botones.c
@@ -10,45 +10,45 @@ int main() {
     int fd;
     struct input_event ev;
 
-    // Open the input device
+    // Abre el dispositivo de entrada
     fd = open("/dev/input/event0", O_RDONLY);
     if (fd == -1) {
         perror("Error opening input device");
         return 1;
     }
 
-    int button_pressed = 0;
+    int is_recording = 0; // Flag para indicar si se está grabando
 
     while (1) {
-        // Read events
+        // Lee eventos
         if (read(fd, &ev, sizeof(struct input_event)) == -1) {
             perror("Error reading input event");
             close(fd);
             return 1;
         }
-         
 
-        // Check for button press event
-        while(ev.type == EV_KEY && ev.value == 1){
-            // Calculate the button number based on the event code
+        // Verifica el evento de presionar botón
+        if (ev.type == EV_KEY) {
             int button_number = ev.code - KEY_RESERVED + 1;
-            int temp_button = button_number;
-            printf("Button %d has been pressed\n", button_number);
-            printf("Value %d \n", ev.value);
-            if(button_number != temp_button ){
+            if (ev.value == 1) { // Botón presionado
+                printf("Button %d has been pressed\n", button_number);
+                if (!is_recording) {
+                    // Iniciar grabación de audio aquí
+                    printf("Recording started\n");
+                    is_recording = 1;
+                }
+            } else if (ev.value == 0) { // Botón liberado
                 printf("Button %d has been released\n", button_number);
-                break;
+                if (is_recording) {
+                    // Finalizar grabación de audio y enviar al destino aquí
+                    printf("Recording stopped and sent to destination\n");
+                    is_recording = 0;
+                }
             }
-            
-        // }
-        // if (ev.type == EV_KEY && ev.value == 1) {
-        //     // Calculate the button number based on the event code
-        //     int button_number = ev.code - KEY_RESERVED + 1;
-        //     printf("Button %d has been pressed\n", button_number);
-        // }
+        }
     }
 
-    // Close the input device (This part of the code will never be reached since the while loop runs indefinitely)
+    // Close the input device
     close(fd);
 
     return 0;
